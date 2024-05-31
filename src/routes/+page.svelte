a91888c3f5cec2462f9638441ee5cc249451e73d
ZakKilpatrick16 <zkk001@latech.edu>
2024-05-31 13:04:02 -0500

    connected supabase to project with drizzle orm

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,23 +2,29 @@
 
 <script lang="ts">
     import { goto } from '$app/navigation';
-    import Layout from './+Layout.svelte'; // Import the Layout component
+    import Layout from './+layout.svelte'; // Import the Layout component
 
     const navigateToPage = (page: string) => {
         goto(`/${page}`);
     };
 </script>
 
+<link href="./src/output.css" rel="stylesheet">
+<link href="./src/app.css" rel="stylesheet">
+
 <Layout> <!-- Wrap your content with the Layout component -->
+    <h1 class="text-6xl font-bold underline">
+        Hello world!
+      </h1>
     <h2>Welcome to the Home Page!</h2>
     <nav class="navbar">
         <ul class="nav-links">
             <li><a href="/Decks">Decks</a></li>
             <li><a href="/Finance">Finance</a></li>
             <li><a href="/TournamentData">Tournament Data</a></li>
-            <li><a href="#">Contact</a></li>
         </ul>
     </nav>
+    
     <div class="name-list">
         <!-- Four clickable names -->
         <button class="name-button" on:click={() => navigateToPage('/Decks')}>Name 1</button>
